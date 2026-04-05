dee43ca3b372c4ac48875d44e158c058123fcc5a
abhishekpandit <abhishekpandit1212@gmail.com>
2026-04-05 14:39:45 +0530

    added/dresscue page

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,24 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import Vixii from './components/Vixii.jsx'
 import BlitzEscape from './components/BlitzEscape.jsx'
 import Dresscue from './components/Dresscue.jsx'
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/vixii" element={<Vixii />} />
